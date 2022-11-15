62d9d25b280d2f0058d8df4d923f3e727f9462d0
李杰 <827188236@qq.com>
2022-11-15 12:02:47 +0800

    commit

--- frontEnd/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-export const baseURL = "http://54.177.233.134:3022";
-
-export const endpoint = 'https://goerli.infura.io/v3/63e8c3efacf140ea90dbbe0604e6e0b7';
-
-export const contracts =  {
-    chainlink:'0x78880dEFC42Fc3bee64F71A480fEc0032Ad6dBA7'
-}
--- frontEnd/src/hooks/useWeb3Context.js
+++
@@ -1,6 +0,0 @@
-import { Web3Context } from "../context/Web3Context";
-import { useContext } from "react";
-
-export default function useWeb3Context() {
-  return useContext(Web3Context);
-}
