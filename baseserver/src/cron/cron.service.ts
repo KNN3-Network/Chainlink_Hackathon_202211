f8e5710b3d1ebac58fddfe94b2ec25a44bfd5abd
ljshwyykl <ljs@gmail.com>
2022-11-17 11:32:38 +0800

    add mumbai

--- baseserver/src/cron/cron.service.ts
+++ baseserver/src/cron/cron.service.ts
@@ -46,9 +46,14 @@ export class CronService {
         progress: 0,
       });
       // Connect to the network
+      // const provider = ethers.getDefaultProvider(
+      //   'https://goerli.infura.io/v3/' +
+      //     this.configService.get('INFURA_API_KEY'),
+      // );
+
+      // mumbai
       const provider = ethers.getDefaultProvider(
-        'https://goerli.infura.io/v3/' +
-          this.configService.get('INFURA_API_KEY'),
+        'https://boldest-few-putty.matic-testnet.quiknode.pro/8ab422cf0f9a1a1a2ec280cb3a39b705fd161e8e/',
       );
 
       // console.log("provider",provider);
@@ -62,6 +67,7 @@ export class CronService {
 
       const wallet = new ethers.Wallet(privateKey, provider);
 
+      /*
       const operatorAddress = '0x1698Dc1EBd90D77792e137802521a3bB06880Db2';
       const requestPageRankInfoJobId = 'f0931bbb43e64ae080265cbea9afeaed';
       const requestPageRankInfoParamsJobId = '5661dacc1693401490405d8e232f6169';
@@ -71,6 +77,21 @@ export class CronService {
       const cronUpkeepFactoryAddress =
         '0x1af3ce8de065774b0ec08942fc5779930d1a9622';
       const registrarAddress = '0x9806cf6fbc89abf286e8140c42174b94836e36f2';
+      const linkTokenAddress = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB';
+
+
+      */
+
+      // mumbai
+      const operatorAddress = '0x86ab46b18a04b6bDe607D50466A798756Ff16576';
+      const requestPageRankInfoJobId = '2d771a437d224f0babb7e10a65c74a5d';
+      const requestPageRankInfoParamsJobId = '0cac20da440b4d5882b8bc6dcd02cbe8';
+      const knn3ProfileClientAddress =
+        '0xF0520c0192D74908930B24B8F76E227548860F7b';
+
+      const cronUpkeepFactoryAddress =
+        '0x7E206B547953BcE9a94B80c28F3f8a869e2D82f9';
+      const registrarAddress = '0xDb8e8e2ccb5C033938736aa89Fe4fa1eDfD15a1d';
       const linkTokenAddress = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB';
 
       let KNN3ProfileClientIface = new ethers.utils.Interface(
@@ -101,7 +122,9 @@ export class CronService {
 
       const tx = await cronUpkeepFactory
         .connect(wallet)
-        .newCronUpkeepWithJob(encodeCronJobString);
+        .newCronUpkeepWithJob(encodeCronJobString, {
+          gasLimit: '10000000',
+        });
       console.log('cronUpkeepFactory', tx);
       const res = await tx.wait();
 
@@ -153,7 +176,10 @@ export class CronService {
 
       const tx1 = await linkToken
         .connect(wallet)
-        .transferAndCall(registrarAddress, amount, abiEncodedBytes);
+        .transferAndCall(registrarAddress, amount, abiEncodedBytes, {
+          gasLimit: 10000000,
+        });
+      console.log('tx1', tx1);
       await tx1.wait();
 
       this.emit({
